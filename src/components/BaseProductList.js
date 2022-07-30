ff3cd2e5ad2a439d3e04ba77fb58e07456633d48
maxdrv <maxdrv.stud@gmail.com>
2022-07-30 22:51:30 +0300

    implemented fetch and display base product list (naive approach)

---
+++ src/components/BaseProductList.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import axios from "axios";
+
+class BaseProductList extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            baseProducts: [],
+            errorMsg: null
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/baseProducts")
+            .then(response => {
+                this.setState({
+                    baseProducts: response.data
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    errorMsg: 'Error while fetching data'
+                })
+            })
+    }
+
+    render() {
+        const {baseProducts, errorMsg} = this.state
+        return (
+            <div>
+                {
+                    baseProducts.length ?
+                        baseProducts.map(product => <div>{product.name}</div>) :
+                        null
+                }
+                { errorMsg ? <div>{errorMsg}</div> : null}
+            </div>
+        );
+    }
+}
+
+export default BaseProductList;
