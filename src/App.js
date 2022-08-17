a6171deb0e022a05b9de72ae814369979eafa76e
maxdrv <maxdrv.stud@gmail.com>
2022-08-17 10:51:05 +0300

    new BaseProductPage implementation, using react hooks, it is fixes problem with not working filters (since there is not more need to refresh page)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BaseProductPage from "./components/BaseProductPage";
+import BaseProductPageNew from "./components/BaseProductPageNew";
 
 function App() {
   return (
     <div className="App">
         <BaseProductPage/>
+        <BaseProductPageNew/>
     </div>
   );
 }
